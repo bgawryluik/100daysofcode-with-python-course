631207096b77f04d29478493d1c90c684815559f
Brent Gawryluik <bgawryluik@appneta.com>
2019-05-03 15:03:11 -0700

    Video examples from Day 1 and 2

---
+++ days/01-03-datetimes/mycode/1-flask/program/routes.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from program import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/100Days')
+def p100Days():
+    return render_template('100Days.html')
